240354f935ea9e86a10dabbfaa23c0db803ca1be
Amit Malhotra <amit08@gmail.com>
2021-11-18 18:28:30 -0500

    modified app

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "McGill"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
