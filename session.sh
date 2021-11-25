516851ab717e630de0b9e386241c8414a2c8e71c
Amit Malhotra <amit08@gmail.com>
2021-11-24 23:53:38 -0500

    session.sh updated

--- session.sh
+++ session.sh
@@ -4,6 +4,9 @@
 #doitlive speed: 2
 #doitlive commentecho: false
 
+# our app isn't running
+http http://localhost:8080
+
 # Let's make sure our cluster is good and running
 kubectl cluster-info
 # Let's install argocd
@@ -23,6 +26,36 @@ kubectl get svc -n argocd
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
 
 # already installed the argocd cli
+
+# create a new app
+argocd app create fastapi-hw --repo https://github.com/tanhaa/fastapi-hw --dest-namespace fastapi-hw --dest-server https://kubernetes.default.svc --path charts/fastapi-hw --project default --revision main
+argocd app get fastapi-hw
+kubectl create namespace fastapi-hw
+argocd app sync fastapi-hw
+argocd app get fastapi-hw
+
+http http://localhost:8080
+
+yes | argocd app delete fastapi-hw
+kubectl delete ns fastapi-hw
+kubectl get namespaces
+
+# create with auto sync
+argocd app create fastapi-hw --repo https://github.com/tanhaa/fastapi-hw --dest-namespace fastapi-hw --dest-server https://kubernetes.default.svc --path charts/fastapi-hw --project default --revision main --sync-policy auto --sync-option CreateNamespace=true
+
+# check app is running
+http http://localhost:8080/items/5 q==somequery
+
+# push some changes
+git add app/main.py
+git commit -m "added new route"
+git push origin add_argo
+
+argocd app sync fastapi-hw
+
+http http://localhost:8080/amit
+
 # cleanup
-kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl delete ns argocd
+#yes | argocd app delete fastapi-hw
+#kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+#kubectl delete ns argocd
