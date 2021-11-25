c6344c2542eedf8ee12ea559e835cd3169b6293e
Amit Malhotra <amit08@gmail.com>
2021-11-25 09:41:04 -0500

    added new route

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/name/{name}")
+@app.get("/user/{name}")
 def read_name(name: str):
     return {"Hello": name}
     
--- session.sh
+++ session.sh
@@ -12,8 +12,7 @@ kubectl cluster-info
 # Let's install argocd
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-# give it a second and check if it's running
-kubectl get pods -n argocd
+
 # by default the service is not exposed with an external IP
 kubectl get svc -n argocd
 # need to access the server, so we wil just quickly patch our argocd service type to loadbalancer
@@ -25,20 +24,16 @@ kubectl get svc -n argocd
 # let's get the password:
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
 
-# already installed the argocd cli
-
 # create a new app
 argocd app create fastapi-hw --repo https://github.com/tanhaa/fastapi-hw --dest-namespace fastapi-hw --dest-server https://kubernetes.default.svc --path charts/fastapi-hw --project default --revision main
 argocd app get fastapi-hw
 kubectl create namespace fastapi-hw
 argocd app sync fastapi-hw
-argocd app get fastapi-hw
 
 http http://localhost:8080
 
 yes | argocd app delete fastapi-hw
 kubectl delete ns fastapi-hw
-kubectl get namespaces
 
 # create with auto sync
 argocd app create fastapi-hw --repo https://github.com/tanhaa/fastapi-hw --dest-namespace fastapi-hw --dest-server https://kubernetes.default.svc --path charts/fastapi-hw --project default --revision main --sync-policy auto --sync-option CreateNamespace=true
@@ -47,7 +42,7 @@ argocd app create fastapi-hw --repo https://github.com/tanhaa/fastapi-hw --dest-
 http http://localhost:8080/items/5 q==somequery
 
 # push some changes
-git add app/main.py
+git add *
 git commit -m "added new route"
 git push origin add_argo
 
